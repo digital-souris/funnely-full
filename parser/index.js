23ed11d8814d5967b4ddc77daf67f0bca8327c23
Stepan <digital-souris@ya.ru>
2021-08-21 18:28:46 +0300

    123

--- parser/index.js
+++ parser/index.js
@@ -28,8 +28,8 @@ db.once('open', async () => {
     app.listen(process.env.PORT, () => {
         console.log(`Сервер запущен ` + process.env.PORT)
     })
-    await parserController.startParseStates()
     await parserController.startParseStatesData()
-    await parserController.findChannels()
+    await parserController.startParseStates()
     await parserController.startParseChannelData()
+    await parserController.findChannels()
 })
